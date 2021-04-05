78caa61a4f1ce6a49af49e562947f6922ec27709
leena72 <leena.sharma72@gmail.com>
2021-04-05 12:31:21 +0530

    grid styling

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react'
-import { View, Button, Text, StyleSheet, FlatList } from 'react-native'
+import { View, Button, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native'
 import { CATEGORIES } from '../data/dummyData'
 
-const renderGridItem = (itemData) => {
-    return <View >
-        <Text>
-            {itemData.item.title}
-        </Text>
-     </View>
-}
+
 const CategoriesScreen = props => {
     console.log("props", props)
+
+    const renderGridItem = (itemData) => {
+        return(
+            <TouchableOpacity style={styles.gridItem}onPress={() => {
+                props.navigation.navigate({ routeName: 'CategoryMealScreen' })
+            }}>
+                <View >
+                    <Text>
+                        {itemData.item.title}
+                    </Text>
+                </View>
+            </TouchableOpacity>
+       
+        );
+ }
+
     return (
         <FlatList keyExtractor={(item, index) => item.id} data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
-        // <View style={styles.screen}>
-        //     <Text>
-        //         Categorries screen
-        //     </Text>
-        //     <Button title="Go To Meal Screen" onPress={()=>{
-        //         props.navigation.navigate({routeName:'CategoryMealScreen'})
-        //         // props.navigation.push('CategoryMealScreen') // another method to navigate
-        //                 // props.navigation.replace('CategoryMealScreen') // replace a screen not add in stack
-        //         }}/>
-        // </View>
     )
 };
 
@@ -31,6 +31,18 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    gridItem: {
+        flex: 1,
+        margin: 15,
+        height: 150,
+        borderRadius:10,
+        borderWidth: 1,
+        borderColor: '#000',
+        alignItems:'center',
+        justifyContent:'center'
+
     }
+
 })
 export default CategoriesScreen
