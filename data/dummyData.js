b001afab47b5962eb8bae16c74dacce55c85471f
leena72 <leena.sharma72@gmail.com>
2021-04-05 12:11:34 +0530

    data render on cateegory screen

---
+++ data/dummyData.js
@@ -0,0 +1,8 @@
+import Category from '../models/category';
+
+export const CATEGORIES=[
+    new Category('c1','INDIAN','#f5428d'),
+    new Category('c2','Italian','#f54242'),
+    new Category('c3','Chinees','#f5a442'),
+    new Category('c4','Light & Lovely','#368dff')
+]
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react'
-import { View, Button, Text, StyleSheet } from 'react-native'
+import { View, Button, Text, StyleSheet, FlatList } from 'react-native'
+import { CATEGORIES } from '../data/dummyData'
 
-const CategoriesScreen = props => { console.log("props",props)
+const renderGridItem = (itemData) => {
+    return <View >
+        <Text>
+            {itemData.item.title}
+        </Text>
+     </View>
+}
+const CategoriesScreen = props => {
+    console.log("props", props)
     return (
-        <View style={styles.screen}>
-            <Text>
-                Categorries screen
-            </Text>
-            <Button title="Go To Meal Screen" onPress={()=>{
-                props.navigation.navigate({routeName:'CategoryMealScreen'})
-                // props.navigation.push('CategoryMealScreen') // another method to navigate
-                        // props.navigation.replace('CategoryMealScreen') // replace a screen not add in stack
-                }}/>
-        </View>
+        <FlatList keyExtractor={(item, index) => item.id} data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
+        // <View style={styles.screen}>
+        //     <Text>
+        //         Categorries screen
+        //     </Text>
+        //     <Button title="Go To Meal Screen" onPress={()=>{
+        //         props.navigation.navigate({routeName:'CategoryMealScreen'})
+        //         // props.navigation.push('CategoryMealScreen') // another method to navigate
+        //                 // props.navigation.replace('CategoryMealScreen') // replace a screen not add in stack
+        //         }}/>
+        // </View>
     )
 };
 
 const styles = StyleSheet.create({
-screen:{
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center'
-}
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
 })
 export default CategoriesScreen
