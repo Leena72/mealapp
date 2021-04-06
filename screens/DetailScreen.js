94f8112c52c9945868dd6191d5504f59856ab8d8
leena72 <leena.sharma72@gmail.com>
2021-04-06 19:58:59 +0530

    hear star

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react'
 import { View, Button, Text, StyleSheet } from 'react-native'
 import { CATEGORIES, MEAL } from '../data/dummyData'
 import HeaderButton from '../components/HeaderButton'
-import {HeaderButtons,Item} from 'react-navigation-header-buttons'
+import {
+    HeaderButtons,
+    Item,
+    HiddenItem,
+    OverflowMenu,
+  } from 'react-navigation-header-buttons';
 
 const DetailScreen = props => {
     const mealID=props.navigation.getParam('mealID');
@@ -25,9 +30,21 @@ DetailScreen.navigationOptions = (navigationData) => {
     console.log("selectedMeal",selectedMeal)
     return{
      headerTitle:selectedMeal.title,
-     headerRight:<HeaderButtons HeaderButtonComponent={HeaderButton}>
-         <Item title='Favorite' iconName='ios-star' onPress={()=>{console.log("mark as favorite")}}/>
-     </HeaderButtons>
+    //  headerRight:<HeaderButtons HeaderButtonComponent={HeaderButton}>
+    //      <Item title='Favorite' iconName='ios-star' onPress={()=>{console.log("mark as favorite")}}/>
+    //      <OverflowMenu
+    //         style={{ marginHorizontal: 10 }}
+            
+    //       >
+    //       </OverflowMenu>
+    //  </HeaderButtons>
+    headerRight:<Button
+    // onPress={navigation.getParam('increaseCount')}
+    title="*"
+    
+    style={styles.starbtn}
+
+  />
     }
 }
 
@@ -36,6 +53,12 @@ screen:{
     flex:1,
     justifyContent:'center',
     alignItems:'center'
+},
+starbtn:{
+    backgroundColor:'#fff',
+    color:'#fff',
+    fontSize:22
+    
 }
 })
 export default DetailScreen
